41fb46e93ebe832936cbbf032b20054158525d7a
Steve Bond <s.bond1@lse.ac.uk>
2013-07-24 16:50:11 +0100

    Update version number properly!

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 20130724;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013072400;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011112900;        // Requires this Moodle version
 $plugin->component = 'block_compile_discussions'; // Full name of the plugin (used for diagnostics)
